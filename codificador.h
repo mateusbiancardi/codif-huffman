78604306450d068a54c17f0551971c3f860ee68c
rafacpovilla <rafacpovilla@gmail.com>
2025-08-06 20:34:03 -0300

    tad codificador

--- codificador.h
+++ codificador.h
@@ -4,10 +4,10 @@
 #include "bitmap.h"
 
 // TAD para um nó da árvore de huffman
-typedef struct NoArvore noArvore;
+typedef struct noArvore NoArvore;
 
 // TAD para o codificador, que encapsula a árvore
-typedef struct Codificador codificador;
+typedef struct codificador Codificador;
 
 /**
  * Cria e inicializa um novo codificador.
@@ -20,19 +20,19 @@ Codificador* criaCodificador();
  * @param codific O ponteiro para o codificador.
  * @param frequencia Array de inteiros com a frequência de cada caractere ASCII (0-255).
  */
-void constroiCodificador(codificador* codific, int* frequencia);
+void constroiCodificador(Codificador* codific, int* frequencia);
 
 /**
  * Gera a tabela de codificação a partir da árvore de Huffman do codificador.
  * @param codific O ponteiro para o codificador.
  * @param codigos Array de bitmaps para armazenar os códigos gerados.
  */
-void geraCodificacao(codificador* codific, bitmap* codigos[256]);
+void geraCodificacao(Codificador* codific, bitmap* codigos[256]);
 
 /**
  * Libera a memória alocada para o codificador e sua árvore de Huffman.
  * @param codific O ponteiro para o codificador.
  */
-void liberaCodificador(codificador* codific);
+void liberaCodificador(Codificador* codific);
 
 #endif /* CODIFICADOR_H_ */
