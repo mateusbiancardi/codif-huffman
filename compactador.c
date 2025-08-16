8a3f72b4450a950a157f69792ba482fcc6984198
Mateus Biancardi <mateusbbdasilva@gmail.com>
2025-08-15 21:41:32 -0300

    fix: escrever em bitmap

--- compactador.c
+++ compactador.c
@@ -27,6 +27,11 @@ static void contaFrequencia(Compactador *c) {
   // le o arquivo em modo read binary
   FILE *arq = fopen(c->arqEntrada, "rb");
 
+  if (arq == NULL) {
+    perror("Erro ao abrir arquivo de entrada em contaFrequencia");
+    exit(EXIT_FAILURE);
+  }
+
   // calcula a frequencia de todos os caracteres
   int caractere;
   while ((caractere = fgetc(arq)) != EOF) {
@@ -37,9 +42,6 @@ static void contaFrequencia(Compactador *c) {
 }
 
 static void constroiArvoreHuffman(Compactador *c) {
-  // Implementação da Etapa 2 e 3: Construir lista e montar a árvore
-  // ... usa c->frequencias, cria a lista, monta a árvore e salva em
-  // c->arvore_huffman ...
 
   Lista *listaPrioridade = criaLista();
 
@@ -55,7 +57,7 @@ static void constroiArvoreHuffman(Compactador *c) {
   Arvore *eof = criaNoFolha(256, 1);
   insereItemOrdenado(listaPrioridade, eof, comparaFrequencia);
 
- int qntdLista = getQuantidadeItemsLista(listaPrioridade);
+  int qntdLista = getQuantidadeItemsLista(listaPrioridade);
   // cria nós internos até sobrar o nó raiz
   while (qntdLista > 1) {
     Arvore *noEsquerdo = removePrimeiroItem(listaPrioridade);
@@ -96,93 +98,89 @@ void encontraCaminho(Arvore *noAtual, char caminhoTemporario[],
 };
 
 static void geraTabelaCodigos(Compactador *c) {
-  //buffer temporario pra armazenar o codigo binario
+  // buffer temporario pra armazenar o codigo binario
   char caminhoTemporario[257];
-    for (int i = 0; i < 257; i++){
-      c->tabelaCodigos[i] = NULL;
-    }
-    
-  //percorre a árvore e encontra os caminhos para preencher a tabela de códigos
+  for (int i = 0; i < 257; i++) {
+    c->tabelaCodigos[i] = NULL;
+  }
+
+  // percorre a árvore e encontra os caminhos para preencher a tabela de códigos
   encontraCaminho(c->arvore, caminhoTemporario, 0, c->tabelaCodigos);
 }
 
+static void escreveCabecalho(Arvore *a, bitmap *bm) {
+  if (ehNoFolha(a)) {
+    // Escreve o bit 1 para indicar que é um nó folha
+    bitmapAppendLeastSignificantBit(bm, 1);
+    // Escreve os 8 bits do caractere
+    int caractere = getCaractere(a);
+    for (int i = 7; i >= 0; i--) {
+      bitmapAppendLeastSignificantBit(bm, (caractere >> i) & 1);
+    }
+  } else {
+    // Escreve o bit 0 para indicar que é um nó interno
+    bitmapAppendLeastSignificantBit(bm, 0);
 
-static void escreveCabecalho(Arvore *a, FILE *arq) {
-  if (ehNoFolha(a)){
-    //escreve 1 pra indicar que é um nó folha
-    fputc('1', arq);
-    //escreve o caracter
-    fputc(getCaractere(a), arq);
-
-    //obs: é usado o fputc ao invés do fprintf pq o fputc escreve um byte
-  } 
-  else{
-    //escreve 0 para indicar que é um nó interno
-    fputc('0', arq);
-    //chama recursivamente pros filhos
-    escreveCabecalho(getEsquerda(a), arq);
-    escreveCabecalho(getDireita(a), arq);
+    escreveCabecalho(getEsquerda(a), bm);
+    escreveCabecalho(getDireita(a), bm);
   }
 }
-
-static void escreveBitsNoArquivo(FILE *arq, char *codigos[257], FILE *arq_orig) {
-  bitmap *bm = bitmapInit(256); //buffer temporario
-
-  int caractere;
-  while ((caractere = fgetc(arq_orig)) != EOF){  //le o arquivo original carcater por caracter até o final
-    char *codigo = codigos[caractere];
-    for (int i = 0; i < strlen(codigo); i++){
-      //cada caracter 0 ou 1 da string é convertido em seu valor numerico e adicionado ao bitmap
-      bitmapAppendLeastSignificantBit(bm, codigo[i] - '0');
-
-      if (bitmapGetLength(bm) == 8){
-        //quando froma um byte completo, escreve no arquivo
-        fputc(bitmapGetContents(bm)[0], arq);
-        bitmapLibera(bm);
-        bm = bitmapInit(256);
-      }
-    }
-  }
-    
-  // Adiciona o pseudo-caractere de final de arquivo (256)
-  char *eof_codigo = codigos[256];
-  for (int i = 0; i < strlen(eof_codigo); i++) {
-    bitmapAppendLeastSignificantBit(bm, eof_codigo[i] - '0');
-    if (bitmapGetLength(bm) == 8) {
-      fputc(bitmapGetContents(bm)[0], arq);
-      bitmapLibera(bm);
-      bm = bitmapInit(256);
-    }
-  }
-    
-  //se depois disso tudo, ainda tiver bits no bitmap (menos de 8), eles formam um byte incompleto e escreve eles no arquivo (são os bits extras)
-  if (bitmapGetLength(bm) > 0){
-    putc(bitmapGetContents(bm)[0], arq);
-  }
-    
-  bitmapLibera(bm);
-}
-
 
 static void escreveArquivoCompactado(Compactador *c) {
-  FILE *arq_saida = fopen(c->arqSaida, "wb");  //abre binario
+  FILE *arq_saida = fopen(c->arqSaida, "wb"); // abre binario
   if (arq_saida == NULL) {
     perror("Erro ao abrir arquivo de saida");
     exit(EXIT_FAILURE);
   }
 
-  //escreve a árvore que é o cabeçalho do arquivo compactado
-  escreveCabecalho(c->arvore, arq_saida);
-
-  //escreve os dados codificados
-  FILE *arq_orig = fopen(c->arqEntrada, "rb");  //le binario
-  if (arq_orig == NULL){
-    perror("Erro ao abrir arquivo de entrada para escrita");
+  // abre o arquivo pra saber o tamanho dele
+  FILE *arq_para_medir = fopen(c->arqEntrada, "rb");
+  if (arq_para_medir == NULL) {
+    perror("Erro ao abrir arquivo de entrada para medir tamanho");
     exit(EXIT_FAILURE);
   }
-  escreveBitsNoArquivo(arq_saida, c->tabelaCodigos, arq_orig);
+
+  // move o cursor para o final do arquivo
+  fseek(arq_para_medir, 0, SEEK_END);
+
+  // pega a posição atual, que é o tamanho em bytes
+  long tamanho_arquivo_bytes = ftell(arq_para_medir);
+
+  fclose(arq_para_medir);
+
+  unsigned int tamanho_max_bits = (tamanho_arquivo_bytes * 8) + (512 * 8);
+
+  bitmap *bm = bitmapInit(tamanho_max_bits);
+
+  // escreve a árvore que é o cabeçalho do arquivo compactado
+  escreveCabecalho(c->arvore, bm);
+
+  FILE *arq_orig = fopen(c->arqEntrada, "rb");
+  if (arq_orig == NULL) {
+    perror("Erro ao abrir arquivo de entrada");
+    exit(EXIT_FAILURE);
+  }
+
+  int caractere;
+  while ((caractere = fgetc(arq_orig)) != EOF) {
+    char *codigo = c->tabelaCodigos[caractere];
+    for (int i = 0; codigo[i] != '\0'; i++) {
+      bitmapAppendLeastSignificantBit(bm, codigo[i] - '0');
+    }
+  }
   fclose(arq_orig);
-    
+
+  // escreve o eof no final
+  char *eof_codigo = c->tabelaCodigos[256];
+  for (int i = 0; eof_codigo[i] != '\0'; i++) {
+    bitmapAppendLeastSignificantBit(bm, eof_codigo[i] - '0');
+  }
+
+  // Calcula quantos bytes completos precisam ser escritos
+  unsigned int total_bytes = (bitmapGetLength(bm) + 7) / 8;
+  fwrite(bitmapGetContents(bm), sizeof(unsigned char), total_bytes, arq_saida);
+
+  bitmapLibera(bm);
   fclose(arq_saida);
 }
 
@@ -191,10 +189,10 @@ Compactador *criaCompactador(const char *caminho_entrada) {
 
   c->arqEntrada = strdup(caminho_entrada);
 
-  // adiciona o .bica para o arquivo compactado
-  c->arqSaida = (char *)malloc(strlen(caminho_entrada) + 5); // .bica + \0
+  // adiciona o .comp para o arquivo compactado
+  c->arqSaida = (char *)malloc(strlen(caminho_entrada) + 5); // .comp + \0
   strcpy(c->arqSaida, caminho_entrada);
-  strcat(c->arqSaida, ".bica");
+  strcat(c->arqSaida, ".comp");
 
   for (int i = 0; i < 256; i++) {
     c->tabelaCodigos[i] = NULL;
@@ -204,21 +202,21 @@ Compactador *criaCompactador(const char *caminho_entrada) {
 };
 
 void executaCompactacao(Compactador *c) {
-  //conta a frequência de cada caracter
+  // conta a frequência de cada caracter
   contaFrequencia(c);
 
-  //constrói a árvore
+  // constrói a árvore
   constroiArvoreHuffman(c);
 
-  //gera a tabela de códigos a partir da árvore
+  // gera a tabela de códigos a partir da árvore
   geraTabelaCodigos(c);
 
-  //escreve o arquivo compactado
+  // escreve o arquivo compactado
   escreveArquivoCompactado(c);
 }
 
-void liberaCompactador(Compactador *c){
-  if (c == NULL){  //caso o ponteiro nao seja valido
+void liberaCompactador(Compactador *c) {
+  if (c == NULL) { // caso o ponteiro nao seja valido
     return;
   }
 
@@ -226,13 +224,12 @@ void liberaCompactador(Compactador *c){
   free(c->arqSaida);
   liberaArvore(c->arvore);
 
-  //pra cada ponteiro nao nulo, libera a string alocada
+  // pra cada ponteiro nao nulo, libera a string alocada
   for (int i = 0; i < 257; i++) {
     if (c->tabelaCodigos[i] != NULL) {
       free(c->tabelaCodigos[i]);
     }
   }
 
-  //libera o compactador
   free(c);
 }
